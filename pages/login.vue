fdd88a834648471872959279672206e8b487ffee
masamichi tozawa <fortube7716@gmail.com>
2024-06-15 23:52:10 +0900

    Verify login authentication

---
+++ pages/login.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>ログイン</h1>
+  <button @click="signIn('github')" type="submit">Githubでログイン</button>
+  <button @click="signIn('google')" type="submit">Googleでログイン</button>
+  <button @click="signOut()" type="submit">ログアウト</button>
+  <p>Status: {{ status }}</p>
+</template>
+
+<script lang="ts" setup>
+definePageMeta({
+  auth: {
+    unauthenticatedOnly: true,
+    navigateAuthenticatedTo: "/home", // 認証済みのリダイレクト先
+  },
+});
+const { status, data, signIn, signOut } = useAuth();
+</script>
