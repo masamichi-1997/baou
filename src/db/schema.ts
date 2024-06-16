1f1b83f605f9d7db2ed5508d972accae04d21369
masamichi tozawa <fortube7716@gmail.com>
2024-06-16 16:33:23 +0900

    add betType table schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,5 +1,6 @@
 import { sql } from "drizzle-orm";
 import {
+  boolean,
   index,
   integer,
   pgEnum,
@@ -18,6 +19,15 @@ export const trackConditionEnum = pgEnum("track_condition", [
   "不良",
 ]);
 
+export const betTypeNameEnum = pgEnum("bet_type_name", [
+  "単勝",
+  "複勝",
+  "ワイド",
+  "馬単",
+  "3連複",
+  "3連単",
+]);
+
 export const raceTrackTable = pgTable(
   "race_track",
   {
@@ -28,8 +38,7 @@ export const raceTrackTable = pgTable(
       .default(sql`now()`),
     updatedAt: timestamp("updated_at", { mode: "string" })
       .notNull()
-      .default(sql`now()`)
-      .$onUpdate(() => sql`now()`),
+      .default(sql`now()`),
   },
   (table) => {
     return {
@@ -49,6 +58,20 @@ export const reaceDetailTable = pgTable("race_detail", {
     .default(sql`now()`),
   updatedAt: timestamp("updated_at", { mode: "string" })
     .notNull()
-    .default(sql`now()`)
-    .$onUpdate(() => sql`now()`),
+    .default(sql`now()`),
+});
+
+export const betType = pgTable("bet_type", {
+  id: serial("id").primaryKey(),
+  raceTrackId: integer("race_track_id").references(() => raceTrackTable.id),
+  betTypeName: betTypeNameEnum("bet_type_name"),
+  ticket: integer("ticket"),
+  amout: integer("amount"),
+  isWin: boolean("is_win").notNull().default(false),
+  createdAt: timestamp("created_at", { mode: "string" })
+    .notNull()
+    .default(sql`now()`),
+  updatedAt: timestamp("updated_at", { mode: "string" })
+    .notNull()
+    .default(sql`now()`),
 });
