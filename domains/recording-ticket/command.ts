104609846457193a95ca7177232ccd985144dc71
masamichi tozawa <fortube7716@gmail.com>
2024-06-21 21:38:47 +0900

    wip: add functionality for inserting records

---
+++ domains/recording-ticket/command.ts
@@ -0,0 +1,17 @@
+import { db } from "~/src/db";
+import { raceDetailTable } from "~/src/db/schema";
+
+export const insertRaceDetail = () => {
+  // return db.insert(raceDetailTable).values({
+  //   raceTrackId: 1,
+  //   distance: 1400,
+  //   cource: "ダート",
+  //   trackCondition: "不良",
+  // }).returning({
+    
+  // })
+};
+
+export const insertBet = () => {
+  
+}
