92307a8728a2017e0a593cffd97e584e422caadb
masamichi tozawa <fortube7716@gmail.com>
2024-06-22 22:06:59 +0900

    delete unused code

--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -10,16 +10,12 @@ export default NuxtAuthHandler({
   providers: [
     // @ts-expect-error You need to use .default here for it to work during SSR. May be fixed via Vite at some point
     GithubProvider.default({
-      // clientId: process.env.GITHUB_CLIENT_ID || process.env.PRODUCTION_GITHUB_CLIENT_ID,
       clientId: process.env.GITHUB_CLIENT_ID,
-      // clientSecret: process.env.GITHUB_CLIENT_SECRET || process.env.PRODUCTION_GITHUB_CLIENT_SECRET,
       clientSecret: process.env.GITHUB_CLIENT_SECRET
     }),
     // @ts-expect-error You need to use .default here for it to work during SSR. May be fixed via Vite at some point
     GoogleProvider.default({
-      // clientId: process.env.GOOGLE_CLIENT_ID! || process.env.PRODUCTION_GOOGLE_CLIENT_ID,
       clientId: process.env.GOOGLE_CLIENT_ID!,
-      // clientSecret: process.env.GOOGLE_CLIENT_SECRET! || process.env.PRODUCTION_GOOGLE_CLIENT_SECRET,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
       authorization: {
         params: {
