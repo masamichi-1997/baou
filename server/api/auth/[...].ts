f233611701c3a9ffaf5c47b09168194bbc726a5a
masamichi tozawa <fortube7716@gmail.com>
2024-06-22 14:07:50 +0900

    add production environment variables

--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -9,13 +9,13 @@ export default NuxtAuthHandler({
   providers: [
     // @ts-expect-error You need to use .default here for it to work during SSR. May be fixed via Vite at some point
     GithubProvider.default({
-      clientId: process.env.GITHUB_CLIENT_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      clientId: process.env.GITHUB_CLIENT_ID || process.env.PRODUCTION_GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET || process.env.PRODUCTION_GITHUB_CLIENT_SECRET,
     }),
     // @ts-expect-error You need to use .default here for it to work during SSR. May be fixed via Vite at some point
     GoogleProvider.default({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      clientId: process.env.GOOGLE_CLIENT_ID! || process.env.PRODUCTION_GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET! || process.env.PRODUCTION_GOOGLE_CLIENT_SECRET,
       authorization: {
         params: {
           prompt: "consent",
